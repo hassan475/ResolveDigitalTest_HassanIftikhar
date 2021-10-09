138f433cc9e3fae1d2f4e0ed8c855cee7dd65888
Hassan Iftikhar <hassan.iftikhar@constellationdealer.com>
2021-10-09 20:07:30 +0500

    Pushing .Net web application code (Entity framework code first)

---
+++ ResolveDigitalTest/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResolveDigitalTest.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ TestProject1/EmployeeTest.cs
@@ -0,0 +1,23 @@
+using Moq;
+using ResolveDigitalTest.Controllers;
+
+namespace TestProject1
+{
+    public class EmployeeTest
+    {
+        public Mock<EmployeesController> mock = new Mock<EmployeesController>();
+
+
+        //[Theory]
+
+        //public void CreateEmployee(Employee obj)
+        //{
+        //    //Arrange
+
+        //    //Act
+
+        //    //Assert
+
+        //}
+    }
+}
